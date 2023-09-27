1428f34e5dc472e48f3cd79cacf7c13736576f6a
mykolagolubyev <golubev.nikolay@gmail.com>
2023-09-27 07:47:08 -0400

    generated html

---
+++ docs/bonsai/toc.js
@@ -0,0 +1,232 @@
+toc = [ {
+  "chapterTitle" : "",
+  "dirName" : "",
+  "items" : [ {
+    "chapterTitle" : "",
+    "pageTitle" : "",
+    "pageMeta" : { },
+    "dirName" : "",
+    "fileName" : "index",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  } ]
+}, {
+  "chapterTitle" : "Guide",
+  "dirName" : "guide",
+  "items" : [ {
+    "chapterTitle" : "Guide",
+    "pageTitle" : "Introduction",
+    "pageMeta" : {
+      "title" : [ "Introduction" ]
+    },
+    "dirName" : "guide",
+    "fileName" : "00-introduction",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Web Apps at 10,000 Feet",
+      "id" : "web-apps-at-10000-feet"
+    }, {
+      "title" : "The Underlying Machinery",
+      "id" : "the-underlying-machinery"
+    } ]
+  }, {
+    "chapterTitle" : "Guide",
+    "pageTitle" : "Virtual-dom",
+    "pageMeta" : {
+      "title" : [ "Virtual-dom" ]
+    },
+    "dirName" : "guide",
+    "fileName" : "01-virtual_dom",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Vdom.Node.t",
+      "id" : "vdom-node-t"
+    }, {
+      "title" : "Vdom.Attr.t",
+      "id" : "vdom-attr-t"
+    }, {
+      "title" : "unit Vdom.Effect.t",
+      "id" : "unit-vdom-effect-t"
+    }, {
+      "title" : "The Underlying Machinery",
+      "id" : "the-underlying-machinery"
+    } ]
+  }, {
+    "chapterTitle" : "Guide",
+    "pageTitle" : "Dynamism",
+    "pageMeta" : {
+      "title" : [ "Dynamism" ]
+    },
+    "dirName" : "guide",
+    "fileName" : "02-dynamism",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Values and computations",
+      "id" : "values-and-computations"
+    }, {
+      "title" : "The scary side of values",
+      "id" : "the-scary-side-of-values"
+    }, {
+      "title" : "Inputs to the graph",
+      "id" : "inputs-to-the-graph"
+    }, {
+      "title" : "Bonsai is a compiler",
+      "id" : "bonsai-is-a-compiler"
+    } ]
+  }, {
+    "chapterTitle" : "Guide",
+    "pageTitle" : "State",
+    "pageMeta" : {
+      "title" : [ "State" ]
+    },
+    "dirName" : "guide",
+    "fileName" : "03-state",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Simple State",
+      "id" : "simple-state"
+    }, {
+      "title" : "Multiple Textboxes",
+      "id" : "multiple-textboxes"
+    }, {
+      "title" : "State Machine",
+      "id" : "state-machine"
+    }, {
+      "title" : "Why should functional programmers be okay with stateful components?",
+      "id" : "why-should-functional-programmers-be-okay-with-stateful-components"
+    } ]
+  }, {
+    "chapterTitle" : "Guide",
+    "pageTitle" : "Forms",
+    "pageMeta" : {
+      "title" : [ "Forms" ]
+    },
+    "dirName" : "guide",
+    "fileName" : "04-forms",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Form.t",
+      "id" : "form-t"
+    }, {
+      "title" : "Combinators",
+      "id" : "combinators"
+    }, {
+      "title" : "Validation Parsing",
+      "id" : "validation-parsing"
+    } ]
+  }, {
+    "chapterTitle" : "Guide",
+    "pageTitle" : "Effect",
+    "pageMeta" : {
+      "title" : [ "Effect" ]
+    },
+    "dirName" : "guide",
+    "fileName" : "05-effect",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Making an Effect",
+      "id" : "making-an-effect"
+    }, {
+      "title" : "Using Effects",
+      "id" : "using-effects"
+    } ]
+  }, {
+    "chapterTitle" : "Guide",
+    "pageTitle" : "Flow",
+    "pageMeta" : {
+      "title" : [ "Flow" ]
+    },
+    "dirName" : "guide",
+    "fileName" : "07-flow",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Components as DAGs",
+      "id" : "components-as-dags"
+    }, {
+      "title" : "match%sub",
+      "id" : "matchsub"
+    }, {
+      "title" : "Bonsai.assoc",
+      "id" : "bonsai-assoc"
+    } ]
+  }, {
+    "chapterTitle" : "Guide",
+    "pageTitle" : "Css",
+    "pageMeta" : {
+      "title" : [ "Css" ]
+    },
+    "dirName" : "guide",
+    "fileName" : "08-css",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Handwritten .css Files",
+      "id" : "handwritten-css-files"
+    }, {
+      "title" : "Vdom.Attr.style",
+      "id" : "vdom-attr-style"
+    }, {
+      "title" : "CSS Ppx",
+      "id" : "css-ppx"
+    }, {
+      "title" : "[~rewrite] flag",
+      "id" : "rewrite-flag"
+    }, {
+      "title" : "Theming through PPX CSS",
+      "id" : "theming-through-ppx-css"
+    } ]
+  }, {
+    "chapterTitle" : "Guide",
+    "pageTitle" : "Edge Triggering",
+    "pageMeta" : {
+      "title" : [ "Edge Triggering" ]
+    },
+    "dirName" : "guide",
+    "fileName" : "09-edge-triggering",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "after_display",
+      "id" : "after_display"
+    }, {
+      "title" : "on_activate / on_deactivate",
+      "id" : "on_activate-on_deactivate"
+    } ]
+  }, {
+    "chapterTitle" : "Guide",
+    "pageTitle" : "URL Routing",
+    "pageMeta" : {
+      "title" : [ "URL Routing" ]
+    },
+    "dirName" : "guide",
+    "fileName" : "10-url-routing",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Handwritten Parse / Unparse",
+      "id" : "handwritten-parse-unparse"
+    }, {
+      "title" : "Typed field Parse / Unparse",
+      "id" : "typed-field-parse-unparse"
+    } ]
+  }, {
+    "chapterTitle" : "Guide",
+    "pageTitle" : "RPCs",
+    "pageMeta" : {
+      "title" : [ "RPCs" ]
+    },
+    "dirName" : "guide",
+    "fileName" : "11-rpcs",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Example 1: Doubling a number",
+      "id" : "example-1-doubling-a-number"
+    }, {
+      "title" : "Polling State RPC",
+      "id" : "polling-state-rpc"
+    }, {
+      "title" : "Example 2: Display the current time",
+      "id" : "example-2-display-the-current-time"
+    }, {
+      "title" : "Example 3: Testing a component that sends RPCs",
+      "id" : "example-3-testing-a-component-that-sends-rpcs"
+    } ]
+  } ]
+} ]
